171fe2d39c1243960a5da6c2d317f652c7f2e052
Jessica French <jessica.c.french6@gmail.com>
2022-08-23 22:02:30 +0000

    can search for fav dessert in fav dessert list, still working on printing rank

---
+++ desserts_match_myinitials.R
@@ -0,0 +1,15 @@
+## Read in csv
+
+library(tidyverse)
+library(here)
+
+my_fav_dessert <- read_csv(here("favorite_desserts.csv"))
+fav_dessert_list <- read_csv(here("iconic_desserts.txt"))
+
+for (i in my_fav_dessert$last_name) {
+  for (j in seq_along(fav_dessert_list$dessert)){
+  if (str_to_lower(i) %in% str_to_lower(fav_dessert_list$dessert)) {
+    rank <- fav_dessert_list$rank[j]
+    print(paste(i, "is ranked", rank ))}
+}
+}
